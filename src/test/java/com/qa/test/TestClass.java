60b406184178f22156dca07117311cab57647ab3
Playwright94 <rchandanm94@gmail.com>
2023-05-06 07:47:13 +0530

    this is fist push to the master

---
+++ src/test/java/com/qa/test/TestClass.java
@@ -0,0 +1,23 @@
+package com.qa.test;
+
+import org.testng.annotations.Test;
+
+public class TestClass {
+	
+	
+	
+	@Test
+	public void test1() {
+		
+		System.out.println("this is test1");
+	}
+	
+	
+	@Test
+      public void test2() {
+		
+		System.out.println("this is test2");
+	}
+
+	
+}
